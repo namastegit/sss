c220102b983d053617008e8e20b4746b4835888f
Akshay Kumar <145667760+namastegit@users.noreply.github.com>
2024-10-01 22:51:44 +0530

    Create appbar.jsx

---
+++ reduxFront/appbar.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+ 
+
+  return <div className='max-w-xl mx-auto'>
+    <Navbar></Navbar> 
+  </div>
+}
+
+export default App
